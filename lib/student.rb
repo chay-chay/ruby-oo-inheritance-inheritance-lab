17850327874b9f10663f326c68324ec68d5b1c0e
Chayanit McCormick <chayanit.chaisri@gmail.com>
2020-11-07 13:23:09 -0500

    commit tree f594fcf3826ce5349dd7283a47da4657a0e6b5a8

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,32 @@
-class Student 
-
+class Student < User
+    attr_accessor :knowledge
+def initialize
+    @knowledge = []
+ 
 end
+
+#Define a method, #learn, that takes in a string and adds that string to the student's @knowledge array.
+def learn(knowledge)
+    @knowledge << knowledge
+  end
+  def knowledge
+    @knowledge
+  end
+end
+
+# class Student < User
+
+#     attr_accessor :knowledge
+  
+#     def initialize(knowledge)
+#       @knowledge = []
+#     end
+  
+#     def learn(knowledge)
+#       @knowledge << knowledge
+#     end
+  
+#     def knowledge
+#       @knowledge
+#     end
+#   end
